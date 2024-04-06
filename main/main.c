d98df73aefbfb0f37201c3ac3d8323b8d016a20c
Jonathan Müller <jo-m@outlook.com>
2024-04-06 19:09:08 +0200

    remove blink stuff from example

---
+++ main/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+static const char *TAG = "example";
+
+void app_main(void)
+{
+    while (1) {
+        ESP_LOGI(TAG, "Hello %d!", 42);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
