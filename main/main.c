3c32a6c108bbe3cf2799e512d25584183b3d0ab2
Jonathan Müller <jo-m@outlook.com>
2024-04-07 19:43:20 +0200

    optimize display drawing

--- main/main.c
+++ main/main.c
@@ -19,10 +19,13 @@ static const char *TAG = "example";
 #define SMALLTV_PRO_LCD_H_RES 240
 #define SMALLTV_PRO_LCD_V_RES 240
 #define SMALLTV_PRO_LCD_COLOR_FORMAT LV_COLOR_FORMAT_RGB565
-#define SMALLTV_PRO_LCD_COLOR_DEPTH_BIT 16  // RGB565 == LV_COLOR_FORMAT_RGB565
+#define SMALLTV_PRO_LCD_COLOR_DEPTH_BIT 16
 #define SMALLTV_PRO_LCD_COLOR_DEPTH_BYTE (SMALLTV_PRO_LCD_COLOR_DEPTH_BIT / 8)
 
-#define SMALLTV_PRO_LCD_PX_CLK_HZ (20 * 1000 * 1000)  // TODO: maybe adjus to 80
+#define SMALLTV_PRO_LCD_MAX_TRANSFER_LINES \
+    80  // SMALLTV_PRO_LCD_V_RES should be a multiple of this.
+#define SMALLTV_PRO_LCD_HOST SPI2_HOST
+#define SMALLTV_PRO_LCD_PX_CLK_HZ (40 * 1000 * 1000)
 
 #define SMALLTV_PRO_LCD_MOSI GPIO_NUM_23
 #define SMALLTV_PRO_LCD_SCLK GPIO_NUM_18
@@ -39,25 +42,14 @@ static const char *TAG = "example";
 
 #define SMALLTV_PRO_TOUCH_PIN GPIO_NUM_32
 
-// TODO: determine pixel byte size... 16? 18b?
-// "The ST7789 is a single-chip controller/driver for 262K-color, graphic type TFT-LCD. It consists
-// of 720 source line and 320 gate line driving circuits. This chip is capable of connecting
-// directly to an external microprocessor, and accepts, 8-bits/9-bits/16-bits/18-bits parallel
-// interface. Display data can be stored in the on-chip display data RAM of 240x320x18 bits. It can
-// perform display data RAM read/write operation with no external operation clock to minimize power
-// consumption. In addition, because of the integrated power supply circuit necessary to drive
-// liquid crystal; it is possible to make a display system with the fewest components."
+static void anim_x_cb(void *var, int32_t v) { lv_obj_set_x(var, v); }
 
-#define SMALLTV_PRO_LCD_HOST SPI2_HOST
+static void anim_size_cb(void *var, int32_t v) { lv_obj_set_size(var, v * 4, v * 4); }
 
-// TODO
-#define SMALLTV_PRO_LVGL_TICK_PERIOD_MS 2
-
-// Since heap since is huge on ESP32, Frame buffer is kept activated by default and no flickering
-// effect…
-
-void example_lvgl_demo_ui(lv_display_t *disp) {
+void lvgl_dummy_ui(lv_display_t *disp) {
     lv_obj_t *scr = lv_display_get_screen_active(disp);
+
+    // Text label
     lv_obj_t *label = lv_label_create(scr);
 
     lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
@@ -65,28 +57,44 @@ void example_lvgl_demo_ui(lv_display_t *disp) {
 
     lv_obj_set_width(label, 240);
     lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);
-}
 
-// static bool lcd_notify_flush_ready(esp_lcd_panel_io_handle_t panel_io,
-// esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
-// {
-//     lv_display_t *disp = (lv_display_t *)user_ctx;
-//     lv_display_flush_ready(disp);
-//     return false;
-// }
+    // Animation
+    lv_obj_t *obj = lv_obj_create(scr);
+    lv_obj_set_style_bg_color(obj, lv_palette_main(LV_PALETTE_RED), 0);
+    lv_obj_set_style_radius(obj, LV_RADIUS_CIRCLE, 0);
+
+    lv_obj_align(obj, LV_ALIGN_LEFT_MID, 10, 0);
+
+    lv_anim_t a;
+    lv_anim_init(&a);
+    lv_anim_set_var(&a, obj);
+    lv_anim_set_values(&a, 10, 40);
+    lv_anim_set_duration(&a, 1000);
+    lv_anim_set_playback_delay(&a, 100);
+    lv_anim_set_playback_duration(&a, 300);
+    lv_anim_set_repeat_delay(&a, 500);
+    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+    lv_anim_set_path_cb(&a, lv_anim_path_ease_in_out);
+
+    lv_anim_set_exec_cb(&a, anim_size_cb);
+    lv_anim_start(&a);
+    lv_anim_set_exec_cb(&a, anim_x_cb);
+    lv_anim_set_values(&a, 10, 240);
+    lv_anim_start(&a);
+}
 
 static void lcd_flush_cb(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map) {
     esp_lcd_panel_handle_t panel_handle = (esp_lcd_panel_handle_t)lv_display_get_user_data(disp);
-    int offsetx1 = area->x1;
-    int offsetx2 = area->x2;
-    int offsety1 = area->y1;
-    int offsety2 = area->y2;
-    esp_lcd_panel_draw_bitmap(panel_handle, offsetx1, offsety1, offsetx2 + 1, offsety2 + 1, px_map);
+    int x1 = area->x1;
+    int x2 = area->x2;
+    int y1 = area->y1;
+    int y2 = area->y2;
+    ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(panel_handle, x1, y1, x2 + 1, y2 + 1, px_map));
 
     lv_display_flush_ready(disp);
 }
 
-static void lcd_increase_lvgl_tick(void *arg) { lv_tick_inc(SMALLTV_PRO_LVGL_TICK_PERIOD_MS); }
+static uint32_t lcd_lvgl_tick_get_cb() { return esp_timer_get_time() / 1000; }
 
 void app_main(void) {
     ESP_LOGI(TAG, "Turn off LCD backlight");
@@ -101,11 +109,12 @@ void app_main(void) {
         .miso_io_num = -1,
         .quadwp_io_num = -1,
         .quadhd_io_num = -1,
-        .max_transfer_sz = 80 * SMALLTV_PRO_LCD_H_RES * SMALLTV_PRO_LCD_COLOR_DEPTH_BYTE,
+        .max_transfer_sz = SMALLTV_PRO_LCD_MAX_TRANSFER_LINES * SMALLTV_PRO_LCD_H_RES *
+                           SMALLTV_PRO_LCD_COLOR_DEPTH_BYTE,
         .flags = SPICOMMON_BUSFLAG_MASTER | SPICOMMON_BUSFLAG_GPIO_PINS,
     };
-    ESP_ERROR_CHECK(spi_bus_initialize(SMALLTV_PRO_LCD_HOST, &buscfg,
-                                       SPI_DMA_CH_AUTO));  // Enable the DMA feature
+
+    ESP_ERROR_CHECK(spi_bus_initialize(SMALLTV_PRO_LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
 
     esp_lcd_panel_io_handle_t io_handle = NULL;
     esp_lcd_panel_io_spi_config_t io_config = {
@@ -140,39 +149,29 @@ void app_main(void) {
 
     ESP_LOGI(TAG, "Initialize LVGL library");
     lv_init();
-    // alloc draw buffers used by LVGL
-    // it's recommended to choose the size of the draw buffer(s) to be at least 1/10 screen sized
+    lv_tick_set_cb(lcd_lvgl_tick_get_cb);
     const size_t buf_sz =
-        SMALLTV_PRO_LCD_H_RES * SMALLTV_PRO_LCD_V_RES * SMALLTV_PRO_LCD_COLOR_DEPTH_BYTE / 10;
+        SMALLTV_PRO_LCD_H_RES * SMALLTV_PRO_LCD_V_RES * SMALLTV_PRO_LCD_COLOR_DEPTH_BYTE / 3;
+    assert(buscfg.max_transfer_sz == buf_sz);
+    ESP_LOGI(TAG, "Free memory: %lu bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "Buf size: %u", buf_sz);
     lv_color_t *buf1 = heap_caps_malloc(buf_sz, MALLOC_CAP_DMA);
     assert(buf1);
     lv_color_t *buf2 = heap_caps_malloc(buf_sz, MALLOC_CAP_DMA);
     assert(buf2);
 
+    ESP_LOGI(TAG, "Initialize LVGL display");
     lv_display_t *disp = lv_display_create(SMALLTV_PRO_LCD_H_RES, SMALLTV_PRO_LCD_V_RES);
     lv_display_set_user_data(disp, (void *)panel_handle);
     lv_display_set_flush_cb(disp, lcd_flush_cb);
     lv_display_set_buffers(disp, buf1, buf2, buf_sz, LV_DISPLAY_RENDER_MODE_PARTIAL);
     lv_display_set_color_format(disp, SMALLTV_PRO_LCD_COLOR_FORMAT);
 
-    ESP_LOGI(TAG, "Install LVGL tick timer");
-    const esp_timer_create_args_t lvgl_tick_timer_args = {.callback = &lcd_increase_lvgl_tick,
-                                                          .name = "lvgl_tick"};
-    esp_timer_handle_t lvgl_tick_timer = NULL;
-    ESP_ERROR_CHECK(esp_timer_create(&lvgl_tick_timer_args, &lvgl_tick_timer));
-    ESP_ERROR_CHECK(
-        esp_timer_start_periodic(lvgl_tick_timer, SMALLTV_PRO_LVGL_TICK_PERIOD_MS * 1000));
-
     ESP_LOGI(TAG, "Display LVGL animation");
-    example_lvgl_demo_ui(disp);
+    lvgl_dummy_ui(disp);
 
     while (1) {
-        // raise the task priority of LVGL and/or reduce the handler period can improve the
-        // performance
-        vTaskDelay(pdMS_TO_TICKS(10));
-
-        // The task running lv_timer_handler should have lower priority than that running
-        // `lv_tick_inc`
-        lv_timer_handler();
+        uint32_t time_till_next_ms = lv_timer_handler();
+        vTaskDelay(pdMS_TO_TICKS(time_till_next_ms));
     }
 }
